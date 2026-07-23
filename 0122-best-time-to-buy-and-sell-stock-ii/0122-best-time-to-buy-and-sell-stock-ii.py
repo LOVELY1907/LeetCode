097306953f819867a3ebe1a38254bb2401936490
LOVELY PAVITHRA G <lovely.g1907@gmail.com>
2026-07-23 23:56:56 +0530

    Time: 3 ms (61.12%), Space: 20.3 MB (85.14%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        # not_holding: max profit when not holding stock
-        # holding: max profit when holding stock
-        not_holding = 0
-        holding = -prices[0]
+        profit = 0
 
         for i in range(1, len(prices)):
-            new_not_holding = max(not_holding, holding + prices[i])
-            new_holding = max(holding, not_holding - prices[i])
-            not_holding = new_not_holding
-            holding = new_holding
+            # Collect every upward price movement
+            if prices[i] > prices[i - 1]:
+                profit += prices[i] - prices[i - 1]
 
-        return not_holding
+        return profit
